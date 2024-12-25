ee98dfee85f0f8b10f2a8df55169ed3aa9cc8702
tshelar <tshelar@asu.edu>
2024-12-24 23:37:43 -0700

    Traversing an array.

---
+++ Arrays/bubblesort.java
@@ -0,0 +1,9 @@
+public class bubblesort{
+    public static void main(String[] args) {
+        int number[] = {2,5,6,7,8,9}; 
+        for (int i = 0;i<=number.length;i++){
+            System.out.println(number[i]);
+        }
+        System.out.println("sdhfl");
+    }
+}
