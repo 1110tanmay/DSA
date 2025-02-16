bfc88a9444c1a3ddff8ea94ba30302958febe008
1110tanmay <1110tanmay@gmail.com>
2025-02-15 20:06:42 -0700

    Declared a static method, program works good.

--- Basics/Mathematics/Countdigits.java
+++ Basics/Mathematics/Countdigits.java
@@ -1,14 +1,18 @@
 package Mathematics;
 import java.util.Scanner;
 public class Countdigits {
-    static int countingDigits(int inp){
-        for (int i = 0;i<=inp;i++){
-            System.out.println("Breaking up the number.");
-        }
-        return inp;
+     static int countingDigits(int inp){
+       int result = 0; 
+       while(inp>0){
+        inp=inp/10;
+        result++;
+       }
+        return result;
     }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-            System.out.println("Placeholder");
+            System.out.println("Enter the number: ");
+                int input = scanner.nextInt();
+                System.out.println("The result is:" + countingDigits(input));
     }
 }
