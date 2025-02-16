389226e0eba32ff90163a4c77c7f117a200eb75a
1110tanmay <1110tanmay@gmail.com>
2025-02-16 15:19:59 -0700

    Created a palindrome function.

--- Basics/Mathematics/Palindrome.java
+++ Basics/Mathematics/Palindrome.java
@@ -1,7 +1,10 @@
 package Mathematics;
 
 public class Palindrome {
+    static boolean Palindrome(int inp){
+        int temp=0;
+        return (temp==inp);
+    }
     public static void main(String[] args) {
-        System.out.println("Hello");     
     } 
 }
