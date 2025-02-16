dad9209b3a88a08f6ef81e044f233401f1d623b5
1110tanmay <1110tanmay@gmail.com>
2025-02-16 16:10:43 -0700

    Function for the check.

--- Basics/Mathematics/Palindrome.java
+++ Basics/Mathematics/Palindrome.java
@@ -3,12 +3,15 @@ package Mathematics;
 public class Palindrome {
     static boolean Palindrome(int inp){
         int temp=0, x=0;
-        
-        while(x!=0){
-            
+            int rev = 0;
+        while(x!=0){ //num 763
+            int store = inp%10; //num now becomes 76 
+                rev = rev *10+store;
+                temp = temp/10;
         }
         return (temp==inp);
     }
     public static void main(String[] args) {
+        
     } 
 }
