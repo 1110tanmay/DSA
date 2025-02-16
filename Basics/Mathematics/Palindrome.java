7d376642d36e61fcf611e462074472c9c4e44057
1110tanmay <1110tanmay@gmail.com>
2025-02-16 16:03:29 -0700

    Adding while statement to Palindrome.

--- Basics/Mathematics/Palindrome.java
+++ Basics/Mathematics/Palindrome.java
@@ -2,7 +2,11 @@ package Mathematics;
 
 public class Palindrome {
     static boolean Palindrome(int inp){
-        int temp=0;
+        int temp=0, x=0;
+        
+        while(x!=0){
+            
+        }
         return (temp==inp);
     }
     public static void main(String[] args) {
