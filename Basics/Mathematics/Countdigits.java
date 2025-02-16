88be4aa3db01627e356d5edaf736d3caad5a712a
1110tanmay <1110tanmay@gmail.com>
2025-02-15 20:17:55 -0700

    Added and fixed the do while loop.

--- Basics/Mathematics/Countdigits.java
+++ Basics/Mathematics/Countdigits.java
@@ -10,9 +10,15 @@ public class Countdigits {
         return result;
     }
     public static void main(String[] args) {
+        int choice;
         Scanner scanner = new Scanner(System.in);
+        do {
             System.out.println("Enter the number: ");
                 int input = scanner.nextInt();
                 System.out.println("The result is:" + countingDigits(input));
+                System.out.println("Do you want to continue, press 1 for yes, 0 for no. ");
+                 choice = scanner.nextInt();
+            } while (choice ==1);
+            scanner.close();
     }
 }
