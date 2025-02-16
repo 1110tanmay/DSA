96e8c6f48f86e1ebd51562db243bff98aa0f9f9c
1110tanmay <1110tanmay@gmail.com>
2025-02-16 16:28:06 -0700

    Palindrome done

--- Basics/Mathematics/Palindrome.java
+++ Basics/Mathematics/Palindrome.java
@@ -1,17 +1,17 @@
 package Mathematics;
 
 public class Palindrome {
-    static boolean Palindrome(int inp){
-        int temp=0, x=0;
+    static boolean isPalindrome(int inp){
+        int temp=inp;
             int rev = 0;
-        while(x!=0){ //num 763
+        while(inp!=0){ //num 763
             int store = inp%10; //num now becomes 76 
                 rev = rev *10+store;
-                temp = temp/10;
+                inp = inp/10;
         }
-        return (temp==inp);
+        return rev==temp;
     }
     public static void main(String[] args) {
-        
+        System.out.println("The result is:" + isPalindrome(763));
     } 
 }
