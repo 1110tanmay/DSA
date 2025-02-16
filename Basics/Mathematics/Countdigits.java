2d4aab1c36bf02ea37cbfcd8080c4b05972cb938
1110tanmay <1110tanmay@gmail.com>
2025-02-15 19:12:41 -0700

    Created a function to implement a for loop.

--- Basics/Mathematics/Countdigits.java
+++ Basics/Mathematics/Countdigits.java
@@ -1,5 +1,14 @@
 package Mathematics;
-
+import java.util.Scanner;
 public class Countdigits {
-    
+    static int countingDigits(int inp){
+        for (int i = 0;i<=inp;i++){
+            System.out.println("Breaking up the number.");
+        }
+        return inp;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+            System.out.println("Placeholder");
+    }
 }
