a70c262d7ced2d5bf9e01da5650c081973fa1d37
1110tanmay <1110tanmay@gmail.com>
2025-02-16 18:00:46 -0700

    Factorial code.

---
+++ Basics/Mathematics/Factorial.java
@@ -0,0 +1,13 @@
+package Mathematics;
+
+public class Factorial {
+    static int isFactorial(int inp){
+        for(int i=0;i<=inp;i++){
+            
+        }
+        return  inp;
+    }
+    public static void main(String[] args) {
+        
+    } 
+}
