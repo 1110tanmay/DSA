7c3299ad48306c75aa598ee382d036798050fb09
Tanmay Shelar <1110tanmay@gmail.com>
2025-02-27 18:18:50 -0700

    Hexadecimal to Binary converter with user inputs.

--- Recursion/RecProb4.java
+++ Recursion/RecProb4.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class RecProb4 {
     static int fun(int n){
         if(n==0){
@@ -8,6 +10,14 @@ public class RecProb4 {
         return n;
     }
     public static void main(String[] args) {
-        System.out.println("Yo!"+ fun(7));
+        int cont;
+        do { 
+        Scanner scanner = new Scanner(System.in); //initialised a scanner object. 
+        System.out.println("Yo! Welcome to Decimal to Binary converter code. Put in the number that you wish to convert to Binary.");
+            int useChoice = scanner.nextInt();
+            fun(useChoice);
+            System.out.println("Do you want to continue or exit? Press 1 to continue and 0 to exit.");
+            cont = scanner.nextInt();
+        } while (cont ==1);
     }
 }
