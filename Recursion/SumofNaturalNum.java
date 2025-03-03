ec61229f46cccd1e4cfde875db05d42fca3d2ba8
Tanmay Shelar <1110tanmay@gmail.com>
2025-03-02 20:25:02 -0700

    Recursive solution for n Natural numbers.

---
+++ Recursion/SumofNaturalNum.java
@@ -0,0 +1,8 @@
+public class SumofNaturalNum {
+    static int sumOfNumbers(int x){
+        return  x;
+    }
+    public static void main(String[] args) {
+        System.out.println("Testing");
+    }
+}
