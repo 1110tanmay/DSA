50758e903cbbfb3481fa0b646f2ad40168157c84
Tanmay Shelar <1110tanmay@gmail.com>
2025-03-04 19:17:02 -0700

    Implemented a for each loop.

---
+++ Random/Foreach.java
@@ -0,0 +1,9 @@
+public class Foreach {
+    public static void main(String[] args) {
+        int [] numbers = {1,3,4,5,6};
+        //Implementing for each loop
+        for (int i : numbers) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ Random/GraphTraverse.java
@@ -0,0 +1,5 @@
+public class GraphTraverse {
+    public static void main(String[] args) {
+        System.out.println("Traversing a graph!");
+    }
+}
