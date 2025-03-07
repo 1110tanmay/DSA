c426bedc2b910c540aed47e347db4c356b3ac065
Tanmay Shelar <1110tanmay@gmail.com>
2025-03-06 22:47:06 -0700

    Created a dynamic array, a function too.

--- Arrays/DelArrayOp.java
+++ Arrays/DelArrayOp.java
@@ -1,6 +1,23 @@
+
+import java.util.ArrayList;
+
 public class DelArrayOp {
+
+    static int dyArray(int number, int position){
+        
+        return 1;
+    }
     
     public static void main(String[] args) {
-        System.out.println("Hello world!"); 
+        System.out.println("Inserting stuff in an array!");
+
+        //Creating Dynamic array using array list
+            ArrayList<Integer> dArrayList = new ArrayList<Integer>();
+            dArrayList.add(12);
+            dArrayList.add(11);
+            dArrayList.add(21);
+            dArrayList.add(23);
+                
+                System.out.println(dArrayList);
     }
 }
