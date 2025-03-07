cd151401f260f9a8cb0b74e1fff432e285846fbe
Tanmay Shelar <1110tanmay@gmail.com>
2025-03-06 21:40:43 -0700

    Implemented an array list.

--- Arrays/ArrayListEx.java
+++ Arrays/ArrayListEx.java
@@ -4,7 +4,8 @@ import java.util.ArrayList;
 public interface ArrayListEx {
     public static void main(String[] args) {
         ArrayList<Integer> exIntCheckArrayList = new ArrayList<Integer>();
-        
-        System.out.println("Testing an arrayList in java.");
+        exIntCheckArrayList.add(21);
+        exIntCheckArrayList.add(24);
+        System.out.println(exIntCheckArrayList);
     }
 }
