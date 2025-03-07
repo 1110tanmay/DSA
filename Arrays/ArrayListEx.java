5fbb81eb62b0dba9fc0cbd06e3bacec5c286f10b
Tanmay Shelar <1110tanmay@gmail.com>
2025-03-06 21:33:25 -0700

    Writing an array list.

---
+++ Arrays/ArrayListEx.java
@@ -0,0 +1,10 @@
+
+import java.util.ArrayList;
+
+public interface ArrayListEx {
+    public static void main(String[] args) {
+        ArrayList<Integer> exIntCheckArrayList = new ArrayList<Integer>();
+        
+        System.out.println("Testing an arrayList in java.");
+    }
+}
