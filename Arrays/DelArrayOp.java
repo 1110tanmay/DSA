45c5cd3e6ab598594a2d00418fd294f116f56020
Tanmay Shelar <1110tanmay@gmail.com>
2025-03-07 12:00:48 -0700

    Updated some stuff on the delete code.

--- Arrays/DelArrayOp.java
+++ Arrays/DelArrayOp.java
@@ -1,4 +1,5 @@
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class DelArrayOp {
@@ -16,13 +17,23 @@ public class DelArrayOp {
         //             dArrayList.get(index+1) = dArrayList.get(index); 
         //     }
         // return 1;
-        System.err.println("Array before inserting the element."+dArrayList);
+        System.err.println("Dynamic: Array before inserting the element."+dArrayList);
         dArrayList.add(number);
         return dArrayList;
+    }
+
+    static int[] statArray(int number1, int position){
+        int [] staticArray = {3,4,5,6,7};
+        System.out.print("Static array: Array before adding elements." + staticArray);
+        int index = position-1;
+        for (int i = index; i<staticArray.length;i++){ //Traverse the loop until the length.
+                
+        }
+        return staticArray;
     }
     
     public static void main(String[] args) {
         System.out.println("Inserting stuff in an array!");
-        System.out.println("After adding element:" + dyArray(11));
+        System.out.println("Dynamic: After adding element:" + dyArray(11));
     }
 }
