fb2e1f144f87a91cf7df4deb8f34fae08817c8b7
1110tanmay <1110tanmay@gmail.com>
2025-05-29 17:48:04 +0530

    Created a Dynamic String array.

--- Arrays/NameArray.java
+++ Arrays/NameArray.java
@@ -1,5 +1,12 @@
+
+import java.util.ArrayList;
+
 public class NameArray {
     public static void main(String[]args){
-        System.out.println("What next?");
+        ArrayList<String> arrayString = new ArrayList<String>();
+        arrayString.add("Tanmay");
+        arrayString.add("Shrikant");
+        arrayString.add("Shelar");
+        System.err.println(arrayString);
     }
 }
