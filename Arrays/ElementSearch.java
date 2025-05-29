f3927754956c45c285a59b58646f1d374d2d9d4b
1110tanmay <1110tanmay@gmail.com>
2025-05-29 18:37:19 +0530

    Able to search for an element in the Array.

--- Arrays/ElementSearch.java
+++ Arrays/ElementSearch.java
@@ -1,6 +1,15 @@
 //Search for an element in the array, if found, return the element with its index.
 import java.util.ArrayList;
 public class ElementSearch {
+    //Granting access of the Array to our function.
+    static void checkArray(ArrayList<Integer> intArrayList, int checkNum){
+        int index; //To store index of the variable
+        for (int i = 0;i<intArrayList.size();i++){ //To iterate through the Array
+            if(intArrayList.get(i)==checkNum){
+                System.out.println("The number is present at index:" +intArrayList.get(i));
+            }
+        }
+    }
     public static void main(String[] args) {
     ArrayList<Integer> intArrayList = new ArrayList<>(); //Valid syntax for ArrayList post, Java 7.
     //Dynamic Array with 6 elements
@@ -10,7 +19,7 @@ public class ElementSearch {
     intArrayList.add(25);
     intArrayList.add(30);
     intArrayList.add(35);
-        
-    System.out.println(intArrayList);
-}
+    System.out.println("Entire Array:"+intArrayList);
+    checkArray(intArrayList, 17);
+    }
 }
