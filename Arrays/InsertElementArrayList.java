6945e5dba2305f9c276dcfa047a23c3702204a02
1110tanmay <1110tanmay@gmail.com>
2025-05-30 12:49:20 +0530

    Adding if condition.

---
+++ Arrays/InsertElementArrayList.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+public class InsertElementArrayList {
+    static void insertElement(ArrayList<Integer>inArrayList ,int inElement, int position){
+        
+    }
+    public static void main(String[] args) {
+        ArrayList<Integer> intArrayList = new ArrayList<>(); //Valid syntax for ArrayList post, Java 7.
+        //Dynamic Array with 6 elements
+        intArrayList.add(10);
+        intArrayList.add(15);
+        intArrayList.add(20);
+        intArrayList.add(25);
+        intArrayList.add(30);
+        intArrayList.add(35);
+        System.out.println("Entire Array:"+intArrayList);
+        insertElement(intArrayList ,17,2);
+    }   
+}
