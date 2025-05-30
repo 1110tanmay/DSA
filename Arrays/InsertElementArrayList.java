bdb1c227daad8e9d44ce3862024fa4b37409f5fc
1110tanmay <1110tanmay@gmail.com>
2025-05-30 13:10:12 +0530

    Insert Element done for Dynamic Array.

--- Arrays/InsertElementArrayList.java
+++ Arrays/InsertElementArrayList.java
@@ -1,7 +1,13 @@
 import java.util.ArrayList;
 public class InsertElementArrayList {
-    static void insertElement(ArrayList<Integer>inArrayList ,int inElement, int position){
-        
+    //Dynamic array
+    static void insertElement(ArrayList<Integer>inArrayList ,int inElement, int index){
+        for(int i=index;i<inArrayList.size()-1;i++){ //Start from given index, to the end, -1 to avoid overflowing
+            inArrayList.set(i, inArrayList.get(i+1)); //Moving the element ahead.
+        } //All elements from the position have been moved forward now the index given is vacant.
+        //Will add the element at the given index now
+            inArrayList.set(index, inElement);
+            System.out.println("Dynamic Array implementation:"+inArrayList);
     }
     public static void main(String[] args) {
         ArrayList<Integer> intArrayList = new ArrayList<>(); //Valid syntax for ArrayList post, Java 7.
@@ -12,7 +18,7 @@ public class InsertElementArrayList {
         intArrayList.add(25);
         intArrayList.add(30);
         intArrayList.add(35);
-        System.out.println("Entire Array:"+intArrayList);
-        insertElement(intArrayList ,17,2);
+        System.out.println("Original Array:"+intArrayList);
+        insertElement(intArrayList ,17,1);
     }   
 }
