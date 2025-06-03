178b29b2a3a3ee0de152cdbfebbea3cb9185c54e
1110tanmay <1110tanmay@gmail.com>
2025-06-03 21:03:41 +0530

    Added array to Remove Duplicates.

--- Arrays/RemoveDuplicates.java
+++ Arrays/RemoveDuplicates.java
@@ -1,3 +1,15 @@
+import java.util.ArrayList;
+
 public class RemoveDuplicates {
+static void remDup(){
     
+}
+    public static void main(String[] args) {
+    ArrayList<Integer> inArray = new ArrayList<Integer>();
+    inArray.add(10);
+    inArray.add(20);
+    inArray.add(30);
+    inArray.add(40);
+    System.out.println("Original Array:"+inArray);
+    }
 }
