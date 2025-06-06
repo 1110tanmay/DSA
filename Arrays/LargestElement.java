4a5c70f5ed4e4023e815c004cc91d56e830af7a9
1110tanmay <1110tanmay@gmail.com>
2025-06-06 11:31:25 +0530

    Incorrect LargestElement.java code (fix needed).

--- Arrays/LargestElement.java
+++ Arrays/LargestElement.java
@@ -1,5 +1,27 @@
-//Find the largest element in the Array, and the return the index of it
+//Find the largest number in a given aray
 import java.util.ArrayList;
-public class LargestElement {
-    
+public class LargestElement{
+    static void larArrayCheck(ArrayList<Integer>larArray){
+        int largestElement=0;
+        int currentLargest = larArray.get(0); //initializing current largest to the first element.
+        for(int i=0;i<larArray.size()-1;i++){
+            if(larArray.get(i+1)>larArray.get(i)){
+                currentLargest = larArray.get(i+1);
+            }
+            if(currentLargest>largestElement){
+                currentLargest=largestElement;
+            }
+        }
+        System.err.println("Largest element:"+largestElement);
+    }
+public static void main(String[] args) {
+    ArrayList<Integer> larArray = new ArrayList<Integer>();
+    larArray.add(21);
+    larArray.add(32);
+    larArray.add(58);
+    larArray.add(42);
+    larArray.add(11);
+    System.out.println("Original Array:" + larArray);
+    larArrayCheck(larArray);
+}
 }
