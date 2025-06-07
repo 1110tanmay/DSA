9ac539fee0db0ad1488532c38d6682b281d9057c
1110tanmay <1110tanmay@gmail.com>
2025-06-07 15:45:53 +0530

    Added thought process.

--- Arrays/RemoveDuplicates.java
+++ Arrays/RemoveDuplicates.java
@@ -1,8 +1,15 @@
+//Check for duplicates in the given array, if match found, remove it.
+/*Thought process: 
+ 1. Set current element to the first element. 
+ 2. Traverse through the Array, compare each element with the current element, if match is found update it. 
+ 3. Update current element to i+1 with every iteration. 
+ */
 import java.util.ArrayList;
-
 public class RemoveDuplicates {
 static void remDup(ArrayList<Integer>inArray){
    int currentElement = inArray.get(0);//First element always distinct.
+    
+   System.out.println("Duplicates removed:"+inArray);
 }
     public static void main(String[] args) {
     ArrayList<Integer> inArray = new ArrayList<Integer>();
