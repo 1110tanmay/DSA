32c210a1b16b571c874f01e97ff5351fe3e43fa2
1110tanmay <1110tanmay@gmail.com>
2025-06-16 16:54:18 +0530

    ZerostoEnd.java: Defined regular array.

--- Arrays/ZerostoEnd.java
+++ Arrays/ZerostoEnd.java
@@ -1,3 +1,7 @@
 public class ZerostoEnd {
-    
+    public static void main(String[] args) {
+        //using regular array
+        int [] zToE = {0,13,42,12,0,14,0,19};
+        System.out.println("Original Array:" +zToE);
+    }
 }
