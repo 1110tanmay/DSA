88225de041700c30d8aa89509309333a562f8687
1110tanmay <1110tanmay@gmail.com>
2025-06-17 23:07:00 +0530

    Added LeforRotateByOne.java

---
+++ Arrays/LeftRotateByOne.java
@@ -0,0 +1,11 @@
+//Q. Move all the elements by 1 place. Basically rotate the array by 1. 
+import java.util.Arrays;
+public class LeftRotateByOne {
+    static void leftRotateByOne(int []leftRoByOne){
+    }
+
+    public static void main(String[]args){
+        int [] leftRoByOne = {12,31,14,15,11,10};
+        System.out.println("Original Array:"+Arrays.toString(leftRoByOne));
+    }
+}
