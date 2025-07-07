b6213c8ccef17e5659fd33f5c8b1e86efbf98115
1110tanmay <1110tanmay@gmail.com>
2025-07-07 12:22:19 +0530

    Left Rotate By One done.

--- Arrays/LeftRotateByOne.java
+++ Arrays/LeftRotateByOne.java
@@ -1,11 +1,21 @@
-//Q. Move all the elements by 1 place. Basically rotate the array by 1. 
 import java.util.Arrays;
-public class LeftRotateByOne {
-    static void leftRotateByOne(int []leftRoByOne){
+public class LeftRotateByOne{
+    static void lRotate(int [] numArray){
+        //traverse through the Array
+        int firstElement = numArray[0]; //storing the first element
+        int lastElement = numArray.length-1;
+        System.out.println(firstElement);
+        System.out.println(lastElement);
+        for(int i=0;i<numArray.length-1;i++){
+            numArray[i]=numArray[i+1]; //shifting the element by one
+        }
+        numArray[lastElement] = firstElement; // putting first element in the last index
+        System.out.println("Rotated Array:" + Arrays.toString(numArray));
     }
 
-    public static void main(String[]args){
-        int [] leftRoByOne = {12,31,14,15,11,10};
-        System.out.println("Original Array:"+Arrays.toString(leftRoByOne));
+    public static void main(String[] args) {
+        int [] numArray = {3,16,6,4,10};
+        System.out.println("Original Array:" + Arrays.toString(numArray));
+        lRotate(numArray);
     }
 }
