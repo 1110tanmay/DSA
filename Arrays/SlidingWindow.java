b036870a1fb8e6a3bc0b0c08dc059c3d5e316a69
1110tanmay <1110tanmay@gmail.com>
2025-07-13 17:14:44 +0530

    Sliding window, optimized solution.

--- Arrays/SlidingWindow.java
+++ Arrays/SlidingWindow.java
@@ -14,9 +14,23 @@ public class SlidingWindow {
         }
         System.out.println("Sliding window:" + maxSum);
     }
+    static void slidingWindowOptimized(int [] numArray, int k){
+        int currentSum = 0;
+        int maxSum = 0;
+        //first calculate the sum of the first k elements
+        for (int i=0;i<=k-1;i++){
+            currentSum = currentSum + numArray[i];
+        }
+        for(int j=k;j<numArray.length;j++){
+            currentSum = currentSum + numArray[j] - numArray[j-k] ;
+            maxSum = Math.max(currentSum, maxSum);
+        }
+        System.out.println("Optimized sliding window:"+maxSum);
+    }
     public static void main(String[] args) {
         int [] numArray = {3,2,21,10,15,6};
         System.out.println("Original array:" + Arrays.toString(numArray));
         slidingWindow(numArray, 2);
+        slidingWindowOptimized(numArray, 2);
     }
 }
