8bab7b3c26bc3cc97a06e03e091cb30c87f7e666
1110tanmay <1110tanmay@gmail.com>
2025-07-18 15:02:30 +0530

    Optimized Binary Search added.

--- Searching/BinarySearch.java
+++ Searching/BinarySearch.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 public class BinarySearch {
+    //for unsorted array as well
     static void binSearch(int [] numArray, int num){
         boolean flag = false;
         for(int i=0;i<numArray.length-1;i++){
@@ -12,10 +13,36 @@ public class BinarySearch {
             System.out.println("Element not found.");
         }
     }
+    //For sorted arrays:
+    static void binSearchOptimized(int[] numArraySorted, int num){
+        //At the starting we want our startIndex and endIndex to be the ends of the array.
+        int startIndex = 0;
+        int endIndex = numArraySorted.length-1;  
+        boolean flag = false;  
+            while(startIndex<=endIndex){
+                //We want to keep updating our mid-point everytime
+                int midPoint = (startIndex+endIndex)/2;
+                if(numArraySorted[midPoint] == num){
+                    System.out.println("Number found at:" +midPoint);
+                    flag =true;
+                    break;
+                }
+                else if(num>numArraySorted[midPoint]){
+                    startIndex = midPoint +1;
+                }
+                else{
+                    endIndex = midPoint-1;
+                }
+        }
+        if(flag!=true){
+            System.out.println("Number not found.");
+            }
+    }
     public static void main(String[] args) {
         int [] numArray = {21,23,42,45,1,2,14};
-        System.out.println("Original Array: "+Arrays.toString(numArray));
-        binSearch(numArray, 21);
-        binSearch(numArray, 12);
+        System.out.println("Original unsorted array: "+Arrays.toString(numArray));
+        int [] numArraySorted = {1,23,42,45,47,236,1447};
+        binSearch(numArray, 1);
+        binSearchOptimized(numArraySorted, 1447);
     }
 }
