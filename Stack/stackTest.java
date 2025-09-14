53be1a47d7a6d794e9bfdb4d634303d151762535
1110tanmay <1110tanmay@gmail.com>
2025-09-14 13:03:34 -0700

    Performed peek and pop operation.

--- Stack/stackTest.java
+++ Stack/stackTest.java
@@ -2,6 +2,14 @@
 import java.util.Stack;
 
 public class stackTest {
+
+    static void stackPeekTest (Stack<String> testingStack){
+        System.out.println("Peek operation: " + testingStack.peek());
+    }
+
+    static void stackPopTest(Stack<String> testingStack){
+        System.out.println("Popped string: " + testingStack.peek());
+    }
     public static void main(String[] args) {
         System.out.print("Testing Stack");
         Stack <String> testingStack = new Stack<String>();
@@ -9,6 +17,9 @@ public class stackTest {
         testingStack.push("Shrikant");
         testingStack.push("Swati");
         testingStack.push("Shelar");
-        System.out.println("Printing stack" + testingStack);
+        System.out.println("Entire stack: " + testingStack);
+        stackPeekTest(testingStack);
+        stackPopTest(testingStack);
+        System.out.println("Entire stack after pop: " + testingStack);
     }
 }
