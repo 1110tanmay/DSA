0833c7c7550b76d774521a7b84e19b839c63654a
1110tanmay <1110tanmay@gmail.com>
2025-09-14 12:57:14 -0700

    Printing basic stack done.

---
+++ Stack/stackTest.java
@@ -0,0 +1,14 @@
+
+import java.util.Stack;
+
+public class stackTest {
+    public static void main(String[] args) {
+        System.out.print("Testing Stack");
+        Stack <String> testingStack = new Stack<String>();
+        testingStack.push("Tanmay");
+        testingStack.push("Shrikant");
+        testingStack.push("Swati");
+        testingStack.push("Shelar");
+        System.out.println("Printing stack" + testingStack);
+    }
+}
