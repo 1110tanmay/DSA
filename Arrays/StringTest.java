0e0795e83e5ede7d17f48ddf2b6705e28c0be491
1110tanmay <1110tanmay@gmail.com>
2026-04-05 20:15:23 -0700

    Recursive binary search.

---
+++ Arrays/StringTest.java
@@ -0,0 +1,9 @@
+public class StringTest {
+    String strCheck(String[] carNames){
+        String names;
+        return 
+    }
+    public static void main(String[] args) {
+        String [] carNames = {"Mercedes", "Mazerratti", "Lamborghini", "Alto"};
+    }
+}
--- Searching/BinarySearchRecursive.java
+++ Searching/BinarySearchRecursive.java
@@ -1,13 +1,37 @@
 import java.util.Arrays;
 public class BinarySearchRecursive {
-    static void recursiveBinSearch(int [] numArray, int num){
-        int startIndex =0;
-        int endIndex = numArray.length-1;
+    
+    static int recursiveBinSearch(int [] numArray, int num, int startIndex, int endIndex){
+
+        //base case
+        if(startIndex>endIndex){
+            return 0;
+        }
+        
         int midPoint = (startIndex+endIndex)/2; 
+
+        if(numArray[midPoint] == num){ //Return midpoint
+            return numArray[midPoint];
+        }
+        else if(num<numArray[midPoint]){
+            return recursiveBinSearch(numArray, num, startIndex, midPoint-1);
+        }
+        else if(num>numArray[midPoint]){
+            return recursiveBinSearch(numArray, num, midPoint+1, endIndex);
+        }
+        return -1;
     }
+   
     public static void main(String[] args) {
         int [] numArray = {23,24,25,27,30,48,123};
+        int startIndex = 0;
+        int endIndex = numArray.length-1;
         System.out.println("Original array:" + Arrays.toString(numArray));
-        recursiveBinSearch(numArray, 25);
+        int result = recursiveBinSearch(numArray, 25, 0, endIndex);
+        if(result==-1){
+            System.out.println("Number not found.");
+        }else{
+            System.out.println("Number found:" +result);
+        }
     }
 }
