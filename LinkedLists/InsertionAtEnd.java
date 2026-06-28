a7ef67ea258e36527dbb9079cab7515d44bbde08
1110tanmay <1110tanmay@gmail.com>
2026-06-28 17:14:45 +0530

    Insertion at end works

--- LinkedLists/InsertionAtEnd.java
+++ LinkedLists/InsertionAtEnd.java
@@ -8,8 +8,16 @@ nextNode = null;
 }
 public class InsertionAtEnd {
     static Node insertAtEnd(Node firstNode, int newNode){
-        //
         Node temp = new Node(newNode);
+        //if linkedlist is empty
+        if(firstNode == null){
+            return temp;
+            //Traverse through LinkedList
+        }Node currentNode = firstNode;
+            while(currentNode.nextNode !=null){
+                currentNode  = currentNode.nextNode;
+            }
+            currentNode.nextNode = temp;
       return firstNode;
     }
     static void printLL(Node firstNode){
@@ -26,6 +34,8 @@ public class InsertionAtEnd {
         Node thirdNode = new Node(18);
         firstNode.nextNode = secondNode;
         secondNode.nextNode = thirdNode;
+        printLL(firstNode);
+        insertAtEnd(firstNode, 31);
         printLL(firstNode);
     }
 }
